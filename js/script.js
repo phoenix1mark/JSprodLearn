bcc67024ce17dbb249b8273521f1494bd45491e4
Mark <example@gmail.com>
2020-09-14 01:48:18 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// alert(`Hello`);
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log((answer + 5));
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+
+
+const category = `toys`;
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
